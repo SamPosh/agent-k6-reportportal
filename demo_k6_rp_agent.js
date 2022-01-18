e859d4f60cc1adbcb7d79298fdfee5ce569a4d3b
Viktor Nedelko <viktor.nedelko@getkevin.eu>
2022-01-18 15:51:00 +0200

    Refactor & logs

--- demo_k6_rp_agent.js
+++ demo_k6_rp_agent.js
@@ -1,10 +1,11 @@
-import { startLaunch,finishLaunch } from "./k6ReportPortal.js";
+import { startLaunch, finishLaunch } from "./k6ReportPortal.js";
 import exec from "k6/execution";
 import RpClient from "./k6ReportPortal.js";
+import { sleep } from "k6";
+import http from 'k6/http';
+import { check, fail } from 'k6';
 
-var configFilePath = `./${__ENV.TEST_CONFIG}`
-var testConfig = JSON.parse(open(configFilePath));
-var reporterOptions = testConfig.reporterOptions;
+const reporterOptions = JSON.parse(open(__ENV.TEST_CONFIG)).reporterOptions;
 
 export const options = {
     scenarios: {
@@ -17,31 +18,43 @@ export const options = {
     },
 };
 
-export function setup()
-{
-
-    let launchId = startLaunch(reporterOptions);
+export function setup() {
+    const launchId = startLaunch(reporterOptions);
     console.log(launchId);
-    return {"launchId": launchId}
+    return { "launchId": launchId }
 }
-export default function(data)
-{
+export default function (data) {
+    let logBatch = []; // Defining array for log batch to be uploaded later
     const execIteration = exec.scenario.iterationInTest;
-    let launchId = data.launchId
-    let rpClient = new RpClient(launchId,reporterOptions);
-    let suiteId =rpClient.startSuite(`(Suite) Services #${execIteration+1}`,"Performance Test Workflow");
+    const launchId = data.launchId
+    const rpClient = new RpClient(launchId, reporterOptions);
+    const suiteId = rpClient.startSuite(`(Suite) Services #${execIteration + 1}`, "Performance Test Workflow");
     console.log(suiteId);
-    let testId =rpClient.startTest(suiteId,"(Test) PluginServiceTest","Test Plugin service");
+    const testId = rpClient.startTest(suiteId, "(Test) PluginServiceTest", "Test Plugin service");
     console.log(testId);
-    let uploadPlugintestStepId =rpClient.startTestStep(testId,"(Step) uploadPlugin","(Step) uploadPlugin");
+    const uploadPlugintestStepId = rpClient.startTestStep(testId, "(Step) Check k6 get", "(Step) checkK6get");
+    const res = http.get('http://test.k6.io');
+    if (
+        !check(res, {
+            'status code MUST be 200': (response) => response.status == 200,
+        })
+    ) {
+        rpClient.saveSingleLog('Response was not 200', 'error'); // Save single log
+        fail('status code was *not* 200');
+    }
+    logBatch = rpClient.addLogToBatch(logBatch, 'Response *was* 200', 'info'); // Add first log to batch
     console.log(uploadPlugintestStepId);
-    let updatePlugintestStepId =rpClient.startTestStep(testId,"(Step) updatePlugin","(Step) updatePlugin");
+    const updatePlugintestStepId = rpClient.startTestStep(testId, "(Step) Sleep 1 second", "(Step) Sleep");
+    sleep(1);
+    rpClient.saveSingleLog('Sleep', 'info');
+    logBatch = rpClient.addLogToBatch(logBatch, 'Sleep', 'info'); // Add second log to batch
+    rpClient.saveLogBatch(logBatch); // Upload log batch to the launch
     //finishTestStep(testStepId,"passed");
-    rpClient.finishTestStep(uploadPlugintestStepId,"failed","ab001","(Step) uploadPlugin Failed");
-    rpClient.finishTestStep(updatePlugintestStepId,"passed"); // As passed , it doesn't have issue type and comment
-    rpClient.finishTest(testId,"failed");
+    rpClient.finishTestStep(uploadPlugintestStepId, "failed", "ab001", "(Step) uploadPlugin Failed");
+    rpClient.finishTestStep(updatePlugintestStepId, "passed"); // As passed , it doesn't have issue type and comment
+    rpClient.finishTest(testId, "failed");
     rpClient.finishSuite(suiteId);
 }
 export function teardown(data) {
-   finishLaunch(data.launchId,reporterOptions);
+    finishLaunch(data.launchId, reporterOptions);
 }
--- k6ReportPortal.js
+++ k6ReportPortal.js
@@ -1,4 +1,6 @@
-import http from "k6/http";
+import http from 'k6/http';
+import { uuidv4 } from 'https://jslib.k6.io/k6-utils/1.1.0/index.js';
+import { FormData } from 'https://jslib.k6.io/formdata/0.0.2/index.js';
 
 /**
  * This will create launch in our reportportal project
@@ -8,11 +10,15 @@ import http from "k6/http";
 export function startLaunch(reporterOptions) {
     let reportPortalUri = `${reporterOptions.endpoint}/${reporterOptions.project}`
     let launchName = reporterOptions.launch;
-    let payload = { "name": `${launchName}`, "description": "K6 load test report", "startTime": Date.now(), "mode": "DEFAULT", "attributes": [{ "key": "build", "value": "0.1" }, { "value": "test" }] }
+    let payload = {
+        'name': `${launchName}`,
+        'description': 'K6 load test report',
+        'startTime': Date.now(),
+        'mode': 'DEFAULT',
+        'attributes': [{ 'key': 'build', 'value': '0.1' }, { 'value': 'test' }]
+    }
     let response = http.post(`${reportPortalUri}/launch`, JSON.stringify(payload), getHeader(reporterOptions.token));
-    let body = JSON.parse(response.body);
-    let launchId = body.id;
-    return launchId;
+    return JSON.parse(response.body).id;
 }
 
 /**
@@ -20,14 +26,13 @@ export function startLaunch(reporterOptions) {
  * @param {string} launchId 
  * @param {*} reporterOptions 
  */
-export function finishLaunch(launchId,reporterOptions) {
-    let reportPortalUri = `${reporterOptions.endpoint}/${reporterOptions.project}`
-    let payload = {
-        "endTime": Date.now()
+export function finishLaunch(launchId, reporterOptions) {
+    const reportPortalUri = `${reporterOptions.endpoint}/${reporterOptions.project}`
+    const payload = {
+        'endTime': Date.now()
     }
-    let response = http.put(`${reportPortalUri}/launch/${launchId}/finish`, JSON.stringify(payload), getHeader(reporterOptions.token));
-    let body = JSON.parse(response.body);
-    console.log(`[FinishLaunch] ${body.message}`);
+    const response = http.put(`${reportPortalUri}/launch/${launchId}/finish`, JSON.stringify(payload), getHeader(reporterOptions.token));
+    console.log(`[FinishLaunch] ${JSON.parse(response.body).message}`);
 
 }
 
@@ -37,92 +42,132 @@ export function finishLaunch(launchId,reporterOptions) {
  * @returns 
  */
 function getHeader(token) {
-    var header = {
+    return {
         headers: {
-            "Content-Type": "application/json",
+            'Content-Type': 'application/json',
             authorization: `Bearer ${token}`,
         },
     };
-    return header;
-}
-
+};
 
 /**
  * 
  */
 export default class RpClient {
-
-    constructor(launchId,reporterOptions){
+    constructor(launchId, reporterOptions) {
         this.launchId = launchId
         this.reportPortalUri = `${reporterOptions.endpoint}/${reporterOptions.project}`
         this.token = reporterOptions.token
+    }
 
+    addLogToBatch(jsonObject, message, level = 'error') {
+        let newObject = jsonObject;
+        newObject.push({
+            itemUuid: uuidv4(),
+            message: message,
+            time: Date.now(),
+            launchUuid: this.launchId,
+            level: level
+        });
+        return newObject;
+    }
 
+    saveLogBatch(jsonBody) {
+        console.log(JSON.stringify(jsonBody))
+        let payload = new FormData();
+        payload.append('json_request_part', http.file(JSON.stringify(jsonBody), 'json_request_part', 'application/json'));
+        const response = http.post(`${this.reportPortalUri}/log`, payload.body(),
+            {
+                headers: {
+                    'Content-Type': `multipart/form-data; boundary=${payload.boundary}`,
+                    'Authorization': `Bearer ${reporterOptions.token}`
+                }
+            });
+        console.log(`Following logs uploaded: ${response.body}`)
+    }
+
+    saveSingleLog(message, level = 'error') {
+        const payload = {
+            'message': message,
+            'time': Date.now(),
+            'launchUuid': this.launchId,
+            'level': level
+        }
+        const response = http.post(`${this.reportPortalUri}/log`, JSON.stringify(payload), getHeader(this.token));
+        return JSON.parse(response.body).id;
     }
 
     startSuite(suiteName, suiteDescription) {
-        let payload = {
-            "name": `${suiteName}`, "startTime": Date.now(), "type": "suite", "launchUuid":
-                `${this.launchId}`, "description": `${suiteDescription}`
+        const payload = {
+            'name': suiteName,
+            'startTime': Date.now(),
+            'type': 'suite',
+            'launchUuid': this.launchId,
+            'description': suiteDescription
         }
-        let response = http.post(`${this.reportPortalUri}/item`, JSON.stringify(payload), getHeader(this.token));
-        let body = JSON.parse(response.body);
-        let suiteId = body.id;
-        return suiteId;
-    }
-    startTest( suiteId, testcaseName, testDescription) {
-        let payload = { "name": `${testcaseName}`, "startTime": Date.now(), "type": "test", "launchUuid": `${this.launchId}`, "description": `${testDescription}` }
-        let response = http.post(`${this.reportPortalUri}/item/${suiteId}`, JSON.stringify(payload), getHeader(this.token));
-        let body = JSON.parse(response.body);
-        let testId = body.id;
-        return testId;
+        const response = http.post(`${this.reportPortalUri}/item`, JSON.stringify(payload), getHeader(this.token));
+        return JSON.parse(response.body).id;
     }
 
-    startTestStep( testId, name, description) {
-        let payload = { "name": `${name}`, "startTime": Date.now(), "type": "step", "hasStats": false, "launchUuid": `${this.launchId}`, "description": `${description}` }
-        let response = http.post(`${this.reportPortalUri}/item/${testId}`, JSON.stringify(payload), getHeader(this.token));
-        let body = JSON.parse(response.body);
-        let testStepId = body.id;
-        return testStepId;
+    startTest(suiteId, testcaseName, testDescription) {
+        const payload = {
+            'name': testcaseName,
+            'startTime': Date.now(),
+            'type': 'test',
+            'launchUuid': this.launchId,
+            'description': testDescription
+        }
+        const response = http.post(`${this.reportPortalUri}/item/${suiteId}`, JSON.stringify(payload), getHeader(this.token));
+        return JSON.parse(response.body).id;
     }
 
-    finishTestStep(id, status, issueType, comment = "no comments") {
+    startTestStep(testId, name, description) {
+        const payload = {
+            'name': name,
+            'startTime': Date.now(),
+            'type': 'step',
+            'hasStats': false,
+            'launchUuid': this.launchId,
+            'description': description
+        }
+        const response = http.post(`${this.reportPortalUri}/item/${testId}`, JSON.stringify(payload), getHeader(this.token));
+        return JSON.parse(response.body).id;
+    }
 
+    finishTestStep(id, status, issueType, comment = 'no comments') {
         let payload = {
-            "endTime": Date.now(),
-            "status": `${status}`,
-            "launchUuid": `${this.launchId}`
+            'endTime': Date.now(),
+            'status': status,
+            'launchUuid': this.launchId
         }
         if (issueType !== null) {
-            payload["issue"] =
+            payload['issue'] =
             {
-                "issueType": `${issueType}`,
-                "comment": `${comment}`
+                'issueType': issueType,
+                'comment': comment
             }
         }
-        let response = http.put(`${this.reportPortalUri}/item/${id}`, JSON.stringify(payload), getHeader(this.token));
-        let body = JSON.parse(response.body);
-        console.log(`[FinishTestStep] ${body.message}`);
+        const response = http.put(`${this.reportPortalUri}/item/${id}`, JSON.stringify(payload), getHeader(this.token));
+        console.log(`[FinishTestStep] ${JSON.parse(response.body).message}`);
     }
-    finishTest( id, status) {
-        let payload = {
-            "status": status,
-            "endTime": Date.now(),
-            "launchUuid": `${this.launchId}`
+
+    finishTest(id, status) {
+        const payload = {
+            'status': status,
+            'endTime': Date.now(),
+            'launchUuid': this.launchId
         }
-        let response = http.put(`${this.reportPortalUri}/item/${id}`, JSON.stringify(payload), getHeader(this.token));
-        let body = JSON.parse(response.body);
-        console.log(`[FinishTest]${body.message}`);
+        const response = http.put(`${this.reportPortalUri}/item/${id}`, JSON.stringify(payload), getHeader(this.token));
+        console.log(`[FinishTest]${JSON.parse(response.body).message}`);
 
     }
+
     finishSuite(id) {
-        let payload = {
-            "endTime": Date.now(),
-            "launchUuid": `${this.launchId}`
+        const payload = {
+            'endTime': Date.now(),
+            'launchUuid': this.launchId
         }
-        let response = http.put(`${this.reportPortalUri}/item/${id}`, JSON.stringify(payload), getHeader(this.token));
-        let body = JSON.parse(response.body);
-        console.log(`[FinishTestSuite] ${body.message}`);
+        const response = http.put(`${this.reportPortalUri}/item/${id}`, JSON.stringify(payload), getHeader(this.token));
+        console.log(`[FinishTestSuite] ${JSON.parse(response.body).message}`);
     }
 }
-
